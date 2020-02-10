2982d8e882058429cfc4e21498671587a47f7f63
Paulmc <paulmc@eisystems.ie>
2020-02-10 10:18:35 +0000

    added bit around updating of FiedTags

--- src/api/file/file.resolvers.js
+++ src/api/file/file.resolvers.js
@@ -5,7 +5,6 @@ module.exports = {
   },
   Mutation: {
     createFile: async (parent, args, { req, models }, info) => {
-      console.log("args", args);
       const userId = getUserId(req);
       const user = await models.user.findOne({ _id: userId });
       const { organisation, documentType } = args.data;
@@ -21,6 +20,22 @@ module.exports = {
         return { file };
       } else if (!user) {
         throw new Error("Authentication Required");
+      }
+    },
+    updateFile: async (parent, args, { req, models }, info) => {
+      const userId = getUserId(req);
+      const user = await models.user.findOne({ _id: userId });
+      const { _id, data, fields } = args;
+
+      if (user) {
+        const file = await models.file.findOne({ _id: _id });
+        console.log("file", file);
+        const updatedFile = {
+          ...file
+          //documentType: data.documentType
+        };
+        console.log("updated file", updatedFile);
+        return { updatedFile };
       }
     }
   }
