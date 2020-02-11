6128d7df77c67fc531b405e2385529dc980aee54
Paulmc <paulmc@eisystems.ie>
2020-02-11 21:29:12 +0000

    added static method for find org by name

--- src/api/organisation/organisation.resolvers.js
+++ src/api/organisation/organisation.resolvers.js
@@ -1,21 +1,29 @@
-
-
 module.exports = {
   Query: {
-    Organisations: (parent, args, ctx, info) =>    {
-       console.log('req is ' + ctx.req);   
-      return ctx.models.organisation.find({})
+    Organisations: (parent, args, ctx, info) => {
+      console.log("req is " + ctx.req);
+      return ctx.models.organisation.find({});
+    },
+    Organisation: async (parent, args, ctx, info) => {
+      const { name } = args;
+      return await ctx.models.organisation.findByName({
+        name: name
+      });
+
     }
   },
   Mutation: {
     createOrganisation: async (parent, args, ctx, info) => {
       const { data } = args;
-      //Below is a static method defined in the organisatin model. It will create a new organisation in the db for us, 
+      //Below is a static method defined in the organisatin model. It will create a new organisation in the db for us,
       // as well as create an administrator, encrypt his password and return the expected output for graphql.
-      let result = await ctx.models.organisation.createNewOrganisation({userSchema: ctx.models.user, userData: data});
-      
+      let result = await ctx.models.organisation.createNewOrganisation({
+        userSchema: ctx.models.user,
+        userData: data
+      });
+
       //console.log(test);
       return result;
     }
   }
-}
+};
