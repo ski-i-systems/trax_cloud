41abe0b51f75f631ed1ca841b131d14e76f5df4c
eoingmelly <eoingmelly@gmail.com>
2020-02-11 15:58:28 +0000

    Static and instance methods created to remove code from resolver and place it in the model itself

--- src/api/organisation/organisation.resolvers.js
+++ src/api/organisation/organisation.resolvers.js
@@ -1,4 +1,4 @@
-const { hashPassword } = require("../../utils/hashPassword");
+
 
 module.exports = {
   Query: {
@@ -10,51 +10,12 @@ module.exports = {
   Mutation: {
     createOrganisation: async (parent, args, ctx, info) => {
       const { data } = args;
-
-      let organisation, adminUser;
-
-      console.log('1-Gonna try hash the password');
-      await hashPassword(data.adminPassword)
-      .then(async (hashedPassword) => {
-        console.log('2-Gonna try to create the organisation seeing as we have the password hashed successfully');
-        await ctx.models.organisation.create({
-        name: data.name,
-        active: true
-        })
-        .then(async (createdOrganisation) => {
-          console.log('3-Gonna try create the admin user');
-          await ctx.models.user.create({
-          name: data.adminName,
-          password: hashedPassword,
-          email: data.adminEmail,
-          //Set the organisation id to this user....
-          organisationID: createdOrganisation._id
-          }).catch(err => {
-            console.log('Caught exception when creating admin user with err: ' + err);
-            if(createdOrganisation != undefined)
-              ctx.models.organisation.findOneAndDelete(createdOrganisation);
-          })
-          .then(async (createdUser) => {
-            console.log('4')
-            //Only return the objects if they were both successful. We are potentially
-            //in here even if the create organisation failed, so check it before assigning it.
-            if(createdOrganisation != undefined)
-            {
-              console.log('5')
-              organisation = createdOrganisation;
-              adminUser = createdUser;
-            };
-          })
-          .catch(err => {
-            console.log('Caught exception when creating user with err: ' + err);
-            if(createdOrganisation != undefined)
-              ctx.models.organisation.findOneAndDelete(createdOrganisation);
-          })
-        })
-      })
+      //Below is a static method defined in the organisatin model. It will create a new organisation in the db for us, 
+      // as well as create an administrator, encrypt his password and return the expected output for graphql.
+      let result = await ctx.models.organisation.createNewOrganisation({userSchema: ctx.models.user, userData: data});
       
-      console.log('6')
-      return {organisation, adminUser};
+      //console.log(test);
+      return result;
     }
   }
 }
