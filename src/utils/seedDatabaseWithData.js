948547fc1d9b0611a20864aeddfe9ed29b7c637e
eoingmelly <eoingmelly@gmail.com>
2020-03-11 14:35:36 +0000

    Jest understanding around handling promises, handling global variables, and seeding and deleting db before eacg run of user tests

--- src/utils/seedDatabaseWithData.js
+++ src/utils/seedDatabaseWithData.js
@@ -4,60 +4,103 @@ const Organisation = require('../api/organisation/organisation.model');
 const User = require('../api/user/user.model')
 //let User = mongoose.model("User", userSchema);
 
-const seedDatabaseWithTestData = async () => {
-    //Declare a model of organisation.
+const seedDatabaseWithTestData = () => {
+  //Declare a model of organisation.
     let orgNames = ["Apple","BMW","Chevrolet","Dell","EISystems",/*"Facebook","Google","Honda","Intel","Java","Kellogs","LandRover","Mitsubishi","Nissan","Opel","Peugeot","QualityTractorParts",
 "Renault","Skoda","Toyota","Universal","Volkswagen","Wilson","Yahoo","Xerox"*/];
 
     let firstNames = ["Allison","Virgil","Joe","Andy"/*,"Trent","Fabinho","Jordan","James","Georginio","Roberto","Sadio","Mohamed"*/];
 
-    
-    orgNames.forEach(async element => {
-        //console.log('element is ', element);
-      //Create a new Organisation with data defined by the details passed.
-      let newOrganisation = new Organisation({
-        name: element,
-        active: true
-        });
-        //console.log('newOrg :', newOrganisation);
+    return new Promise( function(resolve, reject) {
+      try{
+      orgNames.forEach(async element =>  {
+        let newOrganisation = new Organisation({
+          name: element,
+          active: true
+          });
 
-       await newOrganisation.save().then(newOrg => {
-            //console.log('newOrg :', newOrg);
-            let user = new User({
-                organisationID: newOrg._id,
-              name: "Jurgen",
-              email: "Jurgen_admin@" + newOrg.name + ".ie",
+          let newOrg = await newOrganisation.save(); 
+          let user = new User({
+            organisationID: newOrg._id,
+            name: "Jurgen",
+            email: "Jurgen_admin@" + newOrg.name + ".ie",
+            password: "12345678"
+          });
+
+          let usr = await user.save();
+          console.log('adminUsr :', usr);
+
+          firstNames.forEach(async name => {
+          let orguser = new User({
+              organisationID: newOrg._id,
+              name: name,
+              email: name + "@" + newOrg.name + ".ie",
               password: "12345678"
             });
-            user.save().then( async usr => {
-                console.log('adminUsr :', usr);
-            }).then(async () => {
-                firstNames.forEach(async name => {
-                    let orguser = new User({
-                        organisationID: newOrg._id,
-                        name: name,
-                        email: name + "@" + newOrg.name + ".ie",
-                        password: "12345678"
-                      });
-                      await orguser.save().then(u => {
-                        console.log(`username: ${u.name} added for org: ${newOrg.name}.`)
-                      });
-                })
-            })
+
+            let u = await orguser.save();
+            console.log(`username: ${u.name} added for org: ${newOrg.name}.`)
+          })
         })
+
+        resolve("Successfully Seeded Database");
+      } catch (ex) {
+        reject(ex);
       }
-    )
-    console.log('should have left the adding part')
+    })
   };
 
-  const clearDatabase = async () => {
-    await Organisation.deleteMany({}).then(async resp => {
-      console.log(resp);
-      await User.deleteMany({}).then(async deleted => {
-        console.log(deleted);
-        return;
-      })
-    });
+const clearDatabase = () => {
+  console.log('I promise to clear database')
+  return new Promise((resolve, reject) => {
+    try{
+      console.log('I promise to clear the users!')
+      Organisation.deleteMany({});
+    
+      console.log('I promise to clear the users!')
+      User.deleteMany({});
+    
+      console.log('I should have done my job by now.');
+      resolve('all Deleted');
+  } catch(err){
+    console.log('the catcher caught it');
+    reject(err);
   }
+});
+}
 
-  module.exports = {seedDatabaseWithTestData, clearDatabase};
+function sum(a, b) {
+  // return new Promise(function(resolve, reject) {
+  //   console.log('in ctor of promise....')
+  //   if(a != null && b != null){
+  //     console.log('about to resolve')
+  //     resolve(a + b);
+  //   } else {
+  //     reject('must pass in numbers');
+  //   }
+  // })
+  return (a + b);
+}
+
+function sumAsync(a, b) {
+  return new Promise(function(resolve, reject) {
+    if(a != null && b != null){
+      console.log('about to resolve ', a , ' plus ', b);
+      resolve(a + b);
+    } else {
+      reject('must pass in numbers');
+    }
+  })
+}
+
+  // const clearDatabase = async () => {
+  //   await Organisation.deleteMany({}).then(async resp => {
+  //     console.log(resp);
+  //     await User.deleteMany({}).then(async deleted => {
+  //       console.log(deleted);
+  //       return;
+  //     })
+  //   });
+  // }
+
+  module.exports = {seedDatabaseWithTestData, clearDatabase, sum, sumAsync};
--- tests/user.test.js
+++ tests/user.test.js
@@ -12,6 +12,9 @@ const client = new ApolloBoost({
   uri: "http://localhost:7777"
 });
 
+//Test Variables....
+let userToken, userOrganisationId;
+
 // async function clearDB(done) {
 //   // const collections = await mongoose.connection.db.collections();
 //   // if (collections) {
@@ -25,91 +28,36 @@ const client = new ApolloBoost({
 //   return done();
 // }
 
-beforeAll(async () => {
-  // await Seeder.seedDatabaseWithTestData().then(() => {
-  //   return;
-  // });
+beforeAll(() => {
 
+   return Seeder.clearDatabase().then((clearDBResult) => {
+    //console.log('result of clearing database is ', clearDBResult);
+    //console.log('So now let\'s try to seed it again with original base data.....');
+    Seeder.seedDatabaseWithTestData().then(seedResult => {
+      //console.log('seedResult is : ', seedResult);
+      //console.log('should have successfully seeded the database and tests can proceed.');
+      // return;
+    }).catch(err => {
+      //console.log('Something has gone wrong, so should we proceed with the tests?', err);
+      // return;
+    })
+  }).catch(err => {
+    //console.log('heres the promise...', theResp);  
+
+    // return;
+  });
+  
 })
 
 beforeEach(async function(done) {
-
-// const orgDetails = {
-//     userData: {
-//       name: "Paul",
-//       adminName: "Paul",
-//       adminEmail: "paul@paulltd.ie",
-//       adminPassword: "12345678"
-//     },
-//     userSchema: userModel
-//   };
-//   const result = await organisationModel.createNewOrganisation(orgDetails);
-
-  if (mongoose.connection.readyState === 0) {
-    mongoose.connect(
-      "mongodb://127.0.0.1:27017/trax_cloud",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
-      },
-      function(err) {
-        if (err) {
-          throw err;
-        }
-        console.log('in here');
-        return done();
-        //return clearDB(done);
-      }
-    );
-  } else {
-    console.log('or here');
-    return done();
-    //return clearDB(done);
-  }
-});
-
-// afterEach(function(done) {
-//   mongoose.disconnect();
-//   return done();
-// });
-
-test("should create organisation with user", async (done) => {
-  const createOrg = gql`
-    mutation {
-      createOrganisation(
-        data: {
-          name: "Paul Ltd"
-          adminName: "PaulMc"
-          adminEmail: "Paulmc@paulltd.ie"
-          adminPassword: "12345678"
-        }
-      ) {
-        organisation {
-          name
-          createdAt
-        }
-        adminUser {
-          name
-          email
-        }
-      }
-    }
-  `;
-  
-  const response = await client.mutate({
-    mutation: createOrg
-  }).then()
-
-  console.log('data is ', response.data);
-  
-  expect(response.data.createOrganisation.organisation.name).toBe("Paul Ltd");
-  expect(response.data.createOrganisation.adminUser.email).toBe(
-    "paulmc@paulltd.ie"
-  );
   return done();
 });
+
+afterEach(function(done) {
+//  mongoose.disconnect();
+  return done();
+});
+
 
 test("should log the user Paulmc in", async (done) => {
   const logPaulIn = gql`
@@ -122,6 +70,7 @@ test("should log the user Paulmc in", async (done) => {
         user{
           name
           email
+          organisationID
         }
       }
     }
@@ -130,9 +79,9 @@ test("should log the user Paulmc in", async (done) => {
     mutation: logPaulIn
   });
 
-console.log(response.data);
+  userOrganisationId = response.data.loginUser.user.organisationID;
+  userToken = response.data.loginUser.token;
 
-  //expect(response.data.loginUser.token).toBe("Paul Ltd");
   expect(response.data.loginUser.user.email).toBe(
     "paulmc@paulltd.ie"
   );
@@ -140,11 +89,173 @@ console.log(response.data);
   return done();
 });
 
+test("should get total user count, expecting 26", async () => {
+  const getUsers = gql`
+    query {
+      Users {
+        name
+        id
+      }
+    }
+  `;
+  const response = await client.query({
+    query: getUsers
+  });
+  console.log('users length ', response.data.Users.length);
+  expect(response.data.Users.length).toBe(26);
+});
+
+test("should get total users in PaulLtd, 2", async () => {
+
+  console.log('userOrganisationId =', userOrganisationId);
+  let idVar = `id: "${userOrganisationId}"`;
+  console.log(idVar);
+
+  const getUsersInPaulsOrg = gql`
+    query{
+      usersByOrg(${idVar}) {
+    id
+    organisationID
+    name
+    email
+    password
+  }
+}
+  `;
+  const response = await client.query({
+    query: getUsersInPaulsOrg
+  });
+  console.log('users ... ', response.data.usersByOrg);
+  expect(response.data.usersByOrg.length).toBe(1);
+});
+
+
+test("1 plus 2 should be 3", () => {
+// return Seeder.sum(1, 2)
+//   .then(re => {
+//   expect(re).toBe(3);    
+//  })
+
+let re = Seeder.sum(1,2);
+  expect(re).toBe(3);
+});
+
+test("1 plus 2 should be 3 even after async", () => {
+  return Seeder.sumAsync(1, 2)
+    .then(re => {
+    expect(re).toBe(3);    
+   })
+  
+  // let re = Seeder.sum(1,2);
+  //   expect(re).toBe(3);
+});  
+
+test("1 plus 1 should be 2", () => {
+// return Seeder.sum(1, 1)
+//     .then(re => {
+//     expect(re).toBe(2);    
+//   })
+
+let re = Seeder.sum(1,1);
+  expect(re).toBe(2);
+});
+
+test("1 plus 1 should be 2 even after async", () => {
+  return Seeder.sumAsync(1, 1)
+      .then(re => {
+      expect(re).toBe(2);    
+    })
+  // let re = Seeder.sum(1,1);
+  //   expect(re).toBe(2);
+});
+  
+test("2 plus 2 should be 4", () => {
+let re = Seeder.sum(2,2);
+expect(re).toBe(4);
+});
+
+test("2 plus 2 should be 4 even after async", () => {
+  let resp = Seeder.sumAsync(2, 2);
+  return resp.then(re => {
+    expect(re).toBe(4);      
+  })
+});
+    
+
+
+
+
+
+// test("should create organisation with user", async (done) => {
+//   const createOrg = gql`
+//     mutation {
+//       createOrganisation(
+//         data: {
+//           name: "Paul Ltd"
+//           adminName: "PaulMc"
+//           adminEmail: "Paulmc@paulltd.ie"
+//           adminPassword: "12345678"
+//         }
+//       ) { 
+//         organisation {
+//           name
+//           createdAt
+//         }
+//         adminUser {
+//           name
+//           email
+//         }
+//       }
+//     }
+//   `;
+  
+//   const response = await client.mutate({
+//     mutation: createOrg
+//   }).then()
+
+//   console.log('data is ', response.data);
+  
+//   expect(response.data.createOrganisation.organisation.name).toBe("Paul Ltd");
+//   expect(response.data.createOrganisation.adminUser.email).toBe(
+//     "paulmc@paulltd.ie"
+//   );
+//   return done();
+// });
+
+// test("should log the user Paulmc in", async (done) => {
+//   const logPaulIn = gql`
+//     mutation{
+//       loginUser(data:{
+//         email:"paulmc@paulltd.ie"
+//         password:"12345678"
+//       }){
+//         token
+//         user{
+//           name
+//           email
+//         }
+//       }
+//     }
+//   `
+//   const response = await client.mutate({
+//     mutation: logPaulIn
+//   });
+
+// console.log(response.data);
+
+//   //expect(response.data.loginUser.token).toBe("Paul Ltd");
+//   expect(response.data.loginUser.user.email).toBe(
+//     "paulmc@paulltd.ie"
+//   );
+
+//   return done();
+// });
+
 afterAll(async done => {
   //await Seeder.clearDatabase();
-console.log('db should be cleared...');
+//console.log('db should be cleared...');
   // clearDB(done);
-  return done();
+  return done(); 
 });
 
 // test("should login with Admin User Created", async () => {
@@ -162,17 +273,4 @@ console.log('db should be cleared...');
 //   expect(response.data.loginUser.user.name).toBe("PaulMc");
 // });
 
-// test("should  user", async () => {
-//   const getUsers = gql`
-//     query {
-//       Users {
-//         name
-//         id
-//       }
-//     }
-//   `;
-//   const response = await client.query({
-//     query: getUsers
-//   });
-//   expect(response.data.Users[1].name).toBe("Anna");
-// });
+
