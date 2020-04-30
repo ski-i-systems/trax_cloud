50807ba7e2cb0d260dc71a03bbb1017a3ee13c94
Paulmc <paulmc@eisystems.ie>
2020-04-30 11:49:04 +0100

    adjusted user model to have firstName,lastName and title

--- src/api/user/user.model.js
+++ src/api/user/user.model.js
@@ -6,10 +6,12 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema(
   {
     organisationID: { type: Schema.Types.ObjectId, ref: "Organisation" },
-    name: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    title: { type: String, required: false },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    active: {type: Boolean, default: true}
+    active: { type: Boolean, default: true },
   },
   { timestamps: true }
 );
@@ -17,22 +19,32 @@ const userSchema = new Schema(
 userSchema.statics.createNewUser = async function(userDetails) {
   //Declare a model of User.
   let User = mongoose.model("User", userSchema);
-  const { name, email, organisationID, password } = userDetails;
+  const { firstName, title,lastName, email, organisationID, password } = userDetails;
   //Create a new User with data defined by the details passed.
 
   //organisationId can be garnered from the logged in user when we are passing it in the request object.
   //Not there at the moment so hardcoding here for the moment
   let emailLower = email.toLowerCase();
 
-  let user = new User({ name, email:emailLower, password, organisationID, active: true });
+  let user = new User({
+    firstName,
+    lastName,
+    title,
+    email: emailLower,
+    password,
+    organisationID,
+    active: true,
+  });
 
   //await hashPassword(password)
-    //.then(async hashedPass => {
-      //user.password = hashedPass;
-    await user.save().then(usr => {
+  //.then(async hashedPass => {
+  //user.password = hashedPass;
+  await user
+    .save()
+    .then((usr) => {
       user = usr;
     })
-    .catch(err => {
+    .catch((err) => {
       throw new Error(err);
     });
 
@@ -42,13 +54,12 @@ userSchema.statics.findUser = async (userId, callback) => {
   const user = mongoose.model("User", userSchema);
   return user.findOne({ _id: userId }, callback);
 };
-userSchema.statics.updateUser = async data => {
+userSchema.statics.updateUser = async (data) => {
   let UserModel = mongoose.model("User", userSchema);
 
-  console.log('data is', data);
+  console.log("data is", data);
 
-
-  data = {...data};
+  data = { ...data };
   return await UserModel.findOneAndUpdate(
     { _id: data.id },
     data,
@@ -65,7 +76,7 @@ userSchema.statics.updateUser = async data => {
   // return User;
 };
 
-userSchema.statics.deleteUser = async userId => {
+userSchema.statics.deleteUser = async (userId) => {
   let UserModel = mongoose.model("User", userSchema);
   let deletedUser = await UserModel.findUser(userId);
   if (deletedUser) {
@@ -79,28 +90,25 @@ userSchema.statics.deleteUser = async userId => {
   return deletedUser;
 };
 
-userSchema.pre('save', async function(next) {
+userSchema.pre("save", async function(next) {
   //console.log('In pre save function of user, applying tolowercase to email field and hashing password if needed');
 
-  if(this.email){
+  if (this.email) {
     this.email = this.email.toLowerCase();
   }
 
-  if(this.password)
-  {
-    if(this.password.length < 25){
+  if (this.password) {
+    if (this.password.length < 25) {
       try {
-        await hashPassword(this.password).then(res => {
+        await hashPassword(this.password).then((res) => {
           this.password = res;
-        })
-      }
-      catch(err){
+        });
+      } catch (err) {
         throw err;
       }
     }
   }
   return next();
 });
-
 
 module.exports = mongoose.model("users", userSchema);
